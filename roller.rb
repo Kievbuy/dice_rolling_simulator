d3ce8f01aa8b2021ba45c9b3faa890776a9b4168
Kievbuy <kievbuy@gmail.com>
2018-01-08 19:14:55 +0200

    add any number of dice

--- roller.rb
+++ roller.rb
@@ -1,5 +1,22 @@
-def roll(sides)
-	rand(sides) + 1
+def roll(sides, number=1)
+	roll_array = []
+	number.times do
+		roll_value = rand(sides) + 1
+		roll_array << roll_value
+	end
+	total = 0
+	roll_array.each do |roll|
+		puts "First Die: #{roll}"
+		new_total = total + roll 
+		total = new_total
+	end
+	puts "Total: #{total}"
 end
 
+puts "We're rolling one six sides die!"
 puts roll(6)
+
+puts "======"
+
+puts "Now we're rolling 4 six sids dice!"
+puts roll(6, 4)
