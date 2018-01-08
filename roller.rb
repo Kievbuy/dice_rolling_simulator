65365cec9c494404fcd5a9270d67762ec17c9744
Kievbuy <kievbuy@gmail.com>
2018-01-08 19:21:47 +0200

    refactor the programm and add the class

--- roller.rb
+++ roller.rb
@@ -1,22 +1,30 @@
-def roll(sides, number=1)
-	roll_array = []
-	number.times do
-		roll_value = rand(sides) + 1
-		roll_array << roll_value
+class Die
+
+	def initialize(sides)
+		@sides = sides
 	end
-	total = 0
-	roll_array.each do |roll|
-		puts "First Die: #{roll}"
-		new_total = total + roll 
-		total = new_total
+
+	def roll(number=1)
+		roll_array = []
+		number.times do
+			roll_value = rand(@sides) + 1
+			roll_array << roll_value
+		end
+		total = 0
+		roll_array.each do |roll|
+			puts "First Die: #{roll}"
+			new_total = total + roll 
+			total = new_total
+		end
+		puts "Total: #{total}"
 	end
-	puts "Total: #{total}"
+
 end
 
 puts "We're rolling one six sides die!"
-puts roll(6)
+puts Die.new(6).roll
 
 puts "======"
 
 puts "Now we're rolling 4 six sids dice!"
-puts roll(6, 4)
+puts Die.new(6).roll(4)
